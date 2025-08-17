374496a0dc46fab538a0344821b624efc8aba5c2
vitalii.b <kiverak@gmail.com>
2025-08-17 15:26:53 +0300

    addStrings

--- src/main/java/SolutionEasy.java
+++ src/main/java/SolutionEasy.java
@@ -1707,4 +1707,28 @@ class SolutionEasy {
         }
         return null;
     }
+
+    public String addStrings(String num1, String num2) {
+        int BASE = 10;
+        StringBuilder sb = new StringBuilder();
+
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+
+        int sum;
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            sum = carry;
+
+            if (i >= 0) sum += num1.charAt(i--) - '0';
+            if (j >= 0) sum += num2.charAt(j--) - '0';
+
+            carry = sum / BASE;
+            sb.append(sum % BASE);
+        }
+
+        if (carry != 0) sb.append(carry);
+
+        return sb.reverse().toString();
+    }
 }
