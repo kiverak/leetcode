0f473b682c086e054459b5b96caaf91f3b0b3416
Vitaliy Borodin <kiverak@gmail.com>
2025-09-01 19:45:16 +0300

    canPlaceFlowers

--- src/main/java/SolutionEasy.java
+++ src/main/java/SolutionEasy.java
@@ -1761,4 +1761,17 @@ class SolutionEasy {
             return max;
         }
     }
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                i++;
+            } else if (flowerbed[i] == 1) {
+                i++;
+            }
+        }
+        return n <= 0;
+    }
 }
